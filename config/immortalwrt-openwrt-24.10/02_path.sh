e79eb77bde4ad3e005bd6e69b9811dadbaedfa07
QMODEM <kakros918@protonmail.com>
2025-04-30 05:24:00 +0800

    Update 02_path.sh

--- config/immortalwrt-openwrt-24.10/02_path.sh
+++ config/immortalwrt-openwrt-24.10/02_path.sh
@@ -17,25 +17,25 @@ curl -o feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/sta
 #############################################
 export WRT_THEME="argon"          # DEFAULT THEME
 export WRT_IP="192.168.1.1"       # DEFAULT IP
-export WRT_MARK="DOTYCAT"     # DORE-V2.0
+export WRT_MARK="DOTYWRT"     # DORE-V2.0
 export WRT_DATE=$(date +%Y%m%d)   # DATE-FORMAT
-export WRT_SSID="DOTYCAT"          # WIFI-NAME
-export WRT_NAME="DOTYCAT"     # HOSTNAME
+export WRT_SSID="DOTYWRT"          # WIFI-NAME
+export WRT_NAME="DOTYWRT"     # HOSTNAME
 
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
-WIFI_SH=$(find ./target/linux/qualcommax/ipq807x/base-files/etc/uci-defaults/ -type f -name "*set-wireless.sh")
-WIFI_UC="./package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc"
+# WIFI_SH=$(find ./target/linux/qualcommax/ipq807x/base-files/etc/uci-defaults/ -type f -name "*set-wireless.sh")
+# WIFI_UC="./package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc"
 
-if [ -f "$WIFI_SH" ]; then
-    sed -i "s/BASE_SSID='.*'/BASE_SSID='$WRT_SSID'/g" $WIFI_SH
-    sed -i "s/BASE_WORD='.*'/BASE_WORD=''/g" $WIFI_SH
-elif [ -f "$WIFI_UC" ]; then
-    sed -i "s/ssid='.*'/ssid='$WRT_SSID'/g" $WIFI_UC
-    sed -i "s/key='.*'/key=''/g" $WIFI_UC
-    sed -i "s/country='.*'/country='MY'/g" $WIFI_UC
-    sed -i "s/encryption='.*'/encryption='none'/g" $WIFI_UC
-fi
+# if [ -f "$WIFI_SH" ]; then
+#     sed -i "s/BASE_SSID='.*'/BASE_SSID='$WRT_SSID'/g" $WIFI_SH
+#     sed -i "s/BASE_WORD='.*'/BASE_WORD=''/g" $WIFI_SH
+# elif [ -f "$WIFI_UC" ]; then
+#     sed -i "s/ssid='.*'/ssid='$WRT_SSID'/g" $WIFI_UC
+#     sed -i "s/key='.*'/key=''/g" $WIFI_UC
+#     sed -i "s/country='.*'/country='MY'/g" $WIFI_UC
+#     sed -i "s/encryption='.*'/encryption='none'/g" $WIFI_UC
+# fi
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
